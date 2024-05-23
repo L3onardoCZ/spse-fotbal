f4afe98dc4572cafb7a3409db64abbcd30a0f5f5
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-05-23 18:18:17 +0200

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import "./css/header.css"
 export default function Header(){
     return(
         <>
-            <img className="absolute w-10 m-4 md:w-32 md:m-10 right-0" src="/spse_logo.webp" alt="" />
+            <img className="absolute w-8 m-4 md:w-28 md:m-10 right-0" src="/spse_logo.webp" alt="" />
             <header className="w-full flex justify-center items-center h-52 md:h-[550px]">
                 <h1 className="text-4xl md:text-8xl font-bold">GO<span className="text-lime-800 antialiased">LETO</span>.</h1>
             </header>
