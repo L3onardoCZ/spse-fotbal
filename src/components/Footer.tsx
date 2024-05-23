1c01367aa118c3d8960761fccfc6d1f699d03368
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-05-23 18:05:13 +0200

    neco

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "./css/footer.css"
+
+export default function Footer(){
+    return(
+        <footer className="w-full flex justify-center h-16 items-center absolute bottom-0">
+            <h3 className="text-md font-medium">© 2024</h3>
+        </footer>
+    )
+}
