73332e086cbba33a8b529df87cd6f99233cbfcab
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-05-23 18:17:01 +0200

    logo spsse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@ import "./css/header.css"
 
 export default function Header(){
     return(
-        <header className="w-full flex justify-center items-center h-52 md:h-[550px]">
-            <h1 className="text-4xl md:text-8xl font-bold">GO<span className="text-lime-800 antialiased">LETO</span>.</h1>
-        </header>
+        <>
+            <img className="absolute w-10 m-4 md:w-32 md:m-10 right-0" src="/spse_logo.webp" alt="" />
+            <header className="w-full flex justify-center items-center h-52 md:h-[550px]">
+                <h1 className="text-4xl md:text-8xl font-bold">GO<span className="text-lime-800 antialiased">LETO</span>.</h1>
+            </header>
+        </>
     )
 }
