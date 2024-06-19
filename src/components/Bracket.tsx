9735184a6b02b53f847af8a664425a7e7f9ab8f9
L3onardo <suchyleon4@gmail.com>
2024-06-19 20:49:28 +0200

    asdasdasd

--- src/components/Bracket.tsx
+++ src/components/Bracket.tsx
@@ -4,7 +4,7 @@ export default function Bracket(){
     return(
       <>
         <div>
-            <iframe src="https://challonge.com/cs/spse_fotbal/module?theme=1" width="100%" height="1000" scrolling="auto"></iframe>
+        <iframe src="https://challonge.com/cs/h641ve9x/module" width="100%" height="1000"scrolling="auto"></iframe>
         </div>
       </>
     )
