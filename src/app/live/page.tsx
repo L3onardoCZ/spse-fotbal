b04ec348c4d4987732c01943dfcc85fe83ffb931
L3onardo <suchyleon4@gmail.com>
2024-06-19 20:01:06 +0200

    adsasdasddas

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-
+import Bracket from "@/components/Bracket"
 
 
 export default function Live(){
     return(
-        <>
-            
+        <> 
+
+            <Bracket />
+
         </>
     )
 }
---
+++ src/components/Bracket.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Bracket(){
+    return(
+      <>
+
+    <iframe src="https://challonge.com/cs/spse_fotbal/module" width="100%" height="1000" scrolling="auto" allowtransparency="true"></iframe>
+      </>
+    )
+}
