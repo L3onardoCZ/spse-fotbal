88b5ba42e6cf650889d9f12f1efd95f37d13461f
L3onardo <suchyleon4@gmail.com>
2024-06-19 21:04:46 +0200

    asddas

--- src/components/Bracket.tsx
+++ src/components/Bracket.tsx
@@ -3,8 +3,8 @@
 export default function Bracket(){
     return(
       <>
-        <div>
-        <iframe src="https://challonge.com/cs/h641ve9x/module" width="100%" height="1000"scrolling="auto"></iframe>
+        <div className="py-28 bg-[#252830]">
+          <iframe src="https://challonge.com/cs/h641ve9x/module?scale_to_fit=1&background=transparent" width="100%" height="1000"scrolling="auto"></iframe>
         </div>
       </>
     )
