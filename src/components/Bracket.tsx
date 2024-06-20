3ef7cc66a3b73e008df532326ea768ba215c6e01
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 08:35:42 +0200

    novy bracket na strance

--- src/components/Bracket.tsx
+++ src/components/Bracket.tsx
@@ -4,7 +4,7 @@ export default function Bracket(){
     return(
       <>
         <div className="py-28 bg-[#252830]">
-          <iframe src="https://challonge.com/cs/h641ve9x/module?scale_to_fit=1&background=transparent" width="100%" height="1000"scrolling="auto"></iframe>
+          <iframe src="https://challonge.com/cs/spse_goleto_turnaj/module?scale_to_fit=1&background=transparent" width="100%" height="1000"scrolling="auto"></iframe>
         </div>
       </>
     )
