3826ed87d74b6767d4628b2ba9cabd52bf3a4c23
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 08:41:55 +0200

    adsijadsoijadios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -26,7 +27,7 @@ export default function RootLayout({
         <meta name="keywords" content="Fotbal, SPSE, SPŠE, Turnaj, Futsal" />
         <meta property="og:title" content="SPŠE & GOLETO Fotbalový Turnaj 2024" />
         <meta property="og:description" content="Oficiální stránka pro školní fotbalový turnaj odehrávájící se 21. června 2024" />
-        <meta property="og:image" content="./plakat.jpg" />
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1017145792315805800/1244332439128510464/A3plakat_SPSE-02.jpg?ex=66750724&is=6673b5a4&hm=d9a4319ef43a3245291c686ee50ed98f5c345969a1ac503a2dedf8ef9a7760ac&=&width=1506&height=1064" />
         <link rel="icon" type="image/x-icon" href="/public/trophy.png" />
         <meta name="theme-color" content="#252830" />
       </head>
@@ -34,6 +35,7 @@ export default function RootLayout({
         <body className={poppins.className}>
           {children}
           <Toaster />
+          <Analytics />
         </body>
 
 
