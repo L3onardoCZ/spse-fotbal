527cb15589cb95e30636ffc504fff00df923643a
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 12:42:37 +0200

    ikonky ke tlacitkum

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,9 @@ import "./css/anton.css";
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
 import { IoMenu } from "react-icons/io5";
+import { FaHome, FaBook } from "react-icons/fa"
+import { IoIosGitBranch } from "react-icons/io"
+import { PiCourtBasketballDuotone } from "react-icons/pi"
 
 import {
     DropdownMenu,
@@ -21,7 +24,7 @@ export default function Navbar(){
             <nav className="w-full flex fixed flex-row justify-between h-auto items-center p-2 md:p-4">
                 <a href="https://spseplzen.cz"><img className="w-12 md:w-14" src="/spse_logo.webp" alt="" /></a>
                 <div className="flex gap-4">
-                <Link href="/" className="hidden md:inline"><Button>Domů</Button></Link>
+                <Link href="/" className="hidden md:inline"><Button><FaHome className="mr-2"/>Domů</Button></Link>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild className="md:hidden">
                         
@@ -31,26 +34,26 @@ export default function Navbar(){
 
                     <DropdownMenuGroup className="flex justify-center flex-col items-center">
                         <DropdownMenuItem>
-                        <Link href="/">Domů</Link>
+                        <FaHome className="mr-2 text-xs"/><Link href="/">Domů</Link>
                         </DropdownMenuItem>
                         
                         <DropdownMenuItem>
-                        <Link href="/pravidla">Pravidla</Link>
+                        <FaBook className="mr-2 text-xs"/><Link href="/pravidla">Pravidla</Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem>
-                        <Link href="/live">Rozpis zápasů</Link>
+                        <IoIosGitBranch className="mr-2 text-xs"/><Link href="/live">Rozpis zápasů</Link>
                         </DropdownMenuItem>
                         <DropdownMenuItem>
-                        <Link href="/rozpisHrist">Rozpis hřišť</Link>
+                        <PiCourtBasketballDuotone className="mr-2 text-xs"/><Link href="/rozpisHrist">Rozpis hřišť</Link>
                         </DropdownMenuItem>
 
 
                     </DropdownMenuGroup>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                <Link href="/pravidla" className="hidden md:inline"><Button variant="secondary">Pravidla</Button></Link>
-                <Link href="/live" className="hidden md:inline"><Button variant="secondary">Rozpis zápasů</Button></Link>
-                <Link href="/rozpisHrist" className="hidden md:inline"><Button variant="secondary">Rozpis hřišť</Button></Link>
+                <Link href="/pravidla" className="hidden md:inline"><Button variant="secondary"><FaBook className="mr-2"/>Pravidla</Button></Link>
+                <Link href="/live" className="hidden md:inline"><Button variant="secondary"><IoIosGitBranch className="mr-2"/>Rozpis zápasů</Button></Link>
+                <Link href="/rozpisHrist" className="hidden md:inline"><Button variant="secondary"><PiCourtBasketballDuotone className="mr-2"/>Rozpis hřišť</Button></Link>
 
                 </div>
                 <a href="https://goleto.cz"><img className="w-12 md:w-14" src="/goleto_logo.svg" alt="" /></a>
