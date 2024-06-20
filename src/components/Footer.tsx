7186447d2db0ef7471fd8cd15d3a158f472c9613
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 11:08:31 +0200

    2.I pridano do footeru

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "./css/footer.css"
 export default function Footer(){
     return(
         <footer className="w-full flex justify-center h-16 items-center fixed bottom-0">
-            <h3 className="text-md font-medium">© 2024</h3>
+            <h3 className="text-md font-medium">&copy; <span className="text-lime-800 font-bold">2.I</span> 2024</h3>
         </footer>
     )
 }
