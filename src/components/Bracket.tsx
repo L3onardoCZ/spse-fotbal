60a026941d7ccb2812f8848879aee33d42c0b35c
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 13:10:05 +0200

    jdasiojaisdojiasdiodasj

--- src/components/Bracket.tsx
+++ src/components/Bracket.tsx
@@ -4,7 +4,7 @@ export default function Bracket(){
     return(
       <>
         <div className="py-28 bg-[#252830]">
-          <iframe src="https://challonge.com/cs/spse_goleto_turnaj/module?scale_to_fit=1&background=transparent" width="100%" height="1000"scrolling="auto"></iframe>
+          <iframe src="https://challonge.com/cs/spse_goleto_turnaj/module?scale_to_fit=1&background=transparent&show_live_status=0&show_standings=1" width="100%" height="1000"scrolling="auto"></iframe>
         </div>
       </>
     )
