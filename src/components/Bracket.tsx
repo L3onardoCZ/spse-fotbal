1db9e2fadfc22e24770595aef4418e11700cc174
L3onardo <75074582+L3onardoCZ@users.noreply.github.com>
2024-06-20 14:57:44 +0200

    better bracket

--- src/components/Bracket.tsx
+++ src/components/Bracket.tsx
@@ -3,8 +3,8 @@
 export default function Bracket(){
     return(
       <>
-        <div className="py-28 bg-[#252830]">
-          <iframe src="https://challonge.com/cs/spse_goleto_turnaj/module?scale_to_fit=1&background=transparent&show_live_status=0&show_standings=1" width="100%" height="1000"scrolling="auto"></iframe>
+        <div className="bg-[#252830]">
+          <iframe className="py-16 md:py-24"src="https://challonge.com/cs/spse_goleto_turnaj/module?scale_to_fit=1&background=transparent&show_live_status=0&show_standings=1" width="100%" height="1000"scrolling="auto"></iframe>
         </div>
       </>
     )
